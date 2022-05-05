6c4f687c48f0cfe5d78684dc2c577ff564a29db5
Eric Hamacher <erichamacher@protonmail.com>
2022-05-04 22:28:44 -0500

    incorporating lamabda start

--- cognito-dev-deploy.sh
+++ cognito-dev-deploy.sh
@@ -1,2 +1,2 @@
-sam deploy --stack-name hvgn-cognito-dev --s3-bucket hvgn-sam-cognito-dev --parameter-overrides Environment=dev --capabilities CAPABILITY_NAMED_IAM
+sam deploy --stack-name hvgn-cognito-dev --s3-bucket hvgn-sam-cognito-dev --parameter-overrides Environment=dev CognitoFromAddress=hvgn-auth@protonmail.com --capabilities CAPABILITY_NAMED_IAM
 
